bb8afb4d0c9d8c3ef5280380818faafaafd90d8c
Oleg <8888kb8888@gmail.com>
2022-11-21 11:27:23 +0300

    test dopol

--- bot.mjs
+++ bot.mjs
@@ -2,9 +2,10 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on('text', msg => msg.reply.text(msg.text))
+//bot.on('text', msg => msg.reply.text(msg.text))
 
 // bot.hears('Привет', (ctx) => ctx.reply('Приветик мой родной'))
 // bot.hears('Как дела?', (ctx) => ctx.reply('Отлично'))
+bot.on('Привет', msg => msg.reply.text(msg.'Как дела?'))
 
 export default bot
