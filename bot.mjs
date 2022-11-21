829d780a51c28a02d7085a89c3dfd64486a8ba4f
Oleg8888ultro <85274310+Oleg8888ultro@users.noreply.github.com>
2022-11-21 17:32:08 +0300

    проба

--- bot.mjs
+++ bot.mjs
@@ -4,7 +4,11 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 // bot.on('text', msg => msg.reply.text(msg.text))
 
-bot.on(['Привет', '/hello'], (msg) => msg.reply.text('Welcome Програмист!'));
+// bot.on(['Привет', '/hello'], (msg) => msg.reply.text('Welcome Програмист!'));
+
+bot.on('/hello', (msg) => {
+    return bot.sendMessage(msg.from.id, `Hello, ${ msg.from.first_name }!`);
+  });
 
 // bot.hears('Привет', (ctx) => ctx.reply('Приветик мой родной'))
 // bot.hears('Как дела?', (ctx) => ctx.reply('Отлично'))
