f3d941f92fae10c19c5cba42b72dd60987ff5dcd
Oleg8888ultro <85274310+Oleg8888ultro@users.noreply.github.com>
2022-11-21 17:18:15 +0300

    dopoln

--- bot.mjs
+++ bot.mjs
@@ -2,7 +2,9 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on('text', msg => msg.reply.text(msg.text))
+// bot.on('text', msg => msg.reply.text(msg.text))
+
+bot.on(['Привет', '/hello'], (msg) => msg.reply.text('Welcome Програмист!'));
 
 // bot.hears('Привет', (ctx) => ctx.reply('Приветик мой родной'))
 // bot.hears('Как дела?', (ctx) => ctx.reply('Отлично'))
